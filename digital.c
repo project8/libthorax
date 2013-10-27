cd7854efa3006c3226eeb23d5bca5f3bda47e69e
Jared Kofron <jared.kofron@gmail.com>
2013-10-27 13:03:24 -0400

    implementation of digital.h

---
+++ digital.c
@@ -0,0 +1,19 @@
+/*
+ * digital.c
+ *
+ * written by jared kofron <jared.kofron@gmail.com>
+ *
+ * documented prototypes are in digital.h
+ */
+#include "digital.h"
+
+// digital to analog
+float fd2a(byte dig) {
+  float scale_pct = (unsigned)(dig)/((1 << (px1500_bits - 1)) - 1);
+  return (float)(px1500_min_val + px1500_range*scale_pct);
+}
+
+double dd2a(byte dig) {
+  double scale_pct = (unsigned)(dig)/((1 << (px1500_bits - 1)) - 1);
+  return (double)(px1500_min_val + px1500_range*scale_pct);
+}
