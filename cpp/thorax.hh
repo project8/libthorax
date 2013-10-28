d34ef6a7a1d44502bbee128d700c2b1d7488a9db
Noah Oblath <nsoblath@mit.edu>
2013-10-28 13:00:41 -0400

    Added c++ header file thorax.hh (intended to be the one header file for the whole library, in case there are multiple C files)

---
+++ cpp/thorax.hh
@@ -0,0 +1,16 @@
+/*
+ * thorax.hh
+ *
+ *  Created on: Oct 28, 2013
+ *      Author: nsoblath
+ */
+
+#ifndef THORAX_HH_
+#define THORAX_HH_
+
+extern "C"
+{
+    #include "digital.h"
+}
+
+#endif /* THORAX_HH_ */
