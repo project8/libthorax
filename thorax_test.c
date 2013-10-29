ac23c645d054c810d9d5af451662e15756afc14f
Noah Oblath <nsoblath@mit.edu>
2013-10-29 16:27:59 -0400

    Added thorax_time.c to define the date/time format string. Added get_absolute_time_str.

--- thorax_test.c
+++ thorax_test.c
@@ -18,6 +18,11 @@ int main()
     struct timespec time;
     get_time_monotonic(&time);
     printf("%" PRIu64 "  %f\n", time_to_nsec(time), time_to_sec(time));
+    printf("date_time_format: %s\n", date_time_format);
+
+    char date_time[512];
+    get_time_absolute_str(date_time);
+    printf("date/time string (UTC): %s\n", date_time);
 
     return 0;
 }
---
+++ thorax_time.c
@@ -0,0 +1,10 @@
+/*
+ * thorax_time.c
+ *
+ *  Created on: Oct 29, 2013
+ *      Author: nsoblath
+ */
+
+#include "thorax_time.h"
+
+char date_time_format[] = "%FT%TZ";
